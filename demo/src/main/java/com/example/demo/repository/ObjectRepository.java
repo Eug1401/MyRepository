9b2b77b70c5810826c34a8d2b0baeee6102a1054
Eug1401 <gismondieugenio.14@gmail.com>
2026-03-09 09:44:16 +0100

    Pulizia repository

--- demo/src/main/java/com/example/demo/repository/ObjectRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.Entity.IncomingMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ObjectRepository extends JpaRepository<IncomingMessage, Long> {
-
-}
