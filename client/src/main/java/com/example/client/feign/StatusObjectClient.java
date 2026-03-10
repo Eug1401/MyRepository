2cae27a7eba117fb867a34e3a332d54fc544b918
Eug1401 <gismondieugenio.14@gmail.com>
2026-03-10 16:13:04 +0100

    aggiunti appunti feign client

--- client/src/main/java/com/example/client/feign/StatusObjectClient.java
+++ client/src/main/java/com/example/client/feign/StatusObjectClient.java
@@ -8,8 +8,14 @@ import java.util.List;
 
 //nell'url va specificato il nome dell'altro servizio nel container (demo -> nel container è app)
 @FeignClient(name="object-status-service", url="http://app:8080")
+//feign client -> strategia per poter effettuare chiamate HTTP verso altre applicazioni in modo semplice
+//anzichè gestire manualmente gli header, il corpo, il body, la response e la request, si realizza un metodo Java che verrà convertito in chiamata direttamente da Spring
 public interface StatusObjectClient {
+
+    //l'interfaccia sarà implementata da Spring che realizzerà tutto dietro le quinte
 
     @GetMapping("/status-object/getAllStatusObject")
     List<GetStatusObjectDTO> getAllStatusObject();
+
+    //la conversione da Json (in arrivo dopo la chiamata) a DTO sarà realizzata da Spring
 }
