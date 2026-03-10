35a236fbac6a35904bc22bc52c9468dab6726cad
Eug1401 <gismondieugenio.14@gmail.com>
2026-03-10 11:18:48 +0100

    aggiunta commento

--- client/src/main/java/com/example/client/dto/GetStatusObjectDTO.java
+++ client/src/main/java/com/example/client/dto/GetStatusObjectDTO.java
@@ -5,6 +5,7 @@ import lombok.Data;
 
 import java.io.Serializable;
 
+//Oggetto che ottengo tramite get all'altro servizio
 @Data
 public class GetStatusObjectDTO implements Serializable {        //deve essere serializzabile per essere salvato da Redis
     private String nome;
